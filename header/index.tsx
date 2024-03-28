72a8b10379e3a38745ccaa9ab0c951417650311a
miroz devkota <mirozxy@gmail.com>
2024-03-28 17:20:10 +0545

    tailwind setup

---
+++ header/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Index() {
+  return (
+    <div className="header">
+      <img
+        src="https://s3.amazonaws.com/cdn.freshdesk.com/data/helpdesk/attachments/production/73004006665/logo/H82WRa3QLQV8vTV1fIPCfc7a5j-vctznFw.png"
+        alt="icon"
+      />
+      NameDrop
+    </div>
+  )
+}
+
+export default Index
